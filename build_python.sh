74d0e5637b21135e64b6802566f0829ab55bab38
ManticoreTechnologies <development@manticore.technology>
2025-05-31 13:15:54 -0400

    Add gui directories

--- build_python.sh
+++ build_python.sh
@@ -1,8 +1,10 @@
 pyinstaller                                 \
 --onefile                                   \
 --name evrmail                              \
+--icon src/evrmail/gui/evrmail_tray_icon.png \
 --add-data "src/evrmail/webui/dist:evrmail/webui/dist"       \
 --add-data "src/evrmail/webui/public:evrmail/webui/public"   \
+--add-data "src/evrmail/gui/icons:evrmail/gui/icons"        \
 --add-data "example.evr.html:."             \
 --add-data "config.json.example:."          \
 evrmail_entry.py
